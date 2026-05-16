427e7671bdf3aed0e47eec627cbfc61fcae9b146
Guilherme <guilhermetr2005@gmail.com>
2026-05-16 15:55:46 -0300

    first commit: estrutura incial do projeto

---
+++ Muttley/src/main/java/br/com/fatec/muttley/Muttley/MuttleyApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.muttley.Muttley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuttleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuttleyApplication.class, args);
+	}
+
+}
---
+++ Muttley/src/test/java/br/com/fatec/muttley/Muttley/MuttleyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.muttley.Muttley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuttleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
